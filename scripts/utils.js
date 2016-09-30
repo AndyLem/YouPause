7d104cce5fca13870692347b5787fe912d577cf3
Andrei Lemzikov <andrei_lemzikov@epam.com>
2016-09-30 13:30:43 +0300

    Refactor notifications to utils

--- scripts/utils.js
+++ scripts/utils.js
@@ -9,3 +9,15 @@ function removeTabFromPausedList(tabId){
     if (index < 0) return;
     pausedTabs.splice(index, 1);
 };
+
+function showNotification(id, message, buttons, iconUrl){
+    chrome.notifications.create(id,
+    {
+        type:"basic",
+        iconUrl: iconUrl || 'images/youtube-pause-128-borders.png',
+        title: "YouTube Pause",
+        message: message,
+        buttons: buttons
+    },
+    function(notificationId) { });
+};
